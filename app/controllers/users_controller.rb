af14e32276085a42b5d0ba9fcdad6bfa979d58c2
github username <github email address>
2019-01-22 15:44:47 -0500

    added user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,60 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @users = User.all
+    end
+
+    def new
+      if !!flash[:data]
+        @user = User.new(flash[:data])
+      else
+        @user = User.new
+      end
+    end
+
+    def create
+     @user= User.create(user_params)
+      if @user.valid?
+       redirect_to user_path(@user)
+      else
+       flash[:errors] = @user.errors.full_messages
+       flash[:data] = user_params
+       redirect_to new_user_path
+      end
+    end
+
+    def show
+      @goals = Goal.all
+    end
+
+    def edit
+    end
+
+    def update
+      if @user.update(user_params)
+       redirect_to user_path(@user)
+      else
+       flash[:errors] = @user.errors.full_messages
+       flash[:data] = user_params
+       redirect_to edit_user_path
+      end
+    end
+
+    def destroy
+      @user.destroy
+      redirect_to users_path
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :password, :email, :photo)
+    end
+
+
 end
